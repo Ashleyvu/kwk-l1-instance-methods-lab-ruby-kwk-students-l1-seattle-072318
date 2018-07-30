c4e9405ffe5bacd6a1a374ef339701830a55a598
Ashley Vu <ashleyvuhoo@gmail.com>
2018-07-30 16:20:53 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
 # dog.rb
+# class Dog
+#   def initialize(bark, sit, person, talk, walk)
+#     @bark = bark
+#     @sit = sit 
+#     @person = person 
+#     @talk = talk 
+#     @walk = walk 
+#   end 
+# end 
+#   dog_one = Dog.new("Woof!","The dog is sitting","Hello")
+# puts dog_one
+
+class Dog 
+  def bark
+    puts "Woof!"
+  end
+  
+  def sit
+  puts "The Dog is sitting"
+  end
+  
+end
+
+
