6c73f47a1ddca24bfe24ad51f148e02e5c84c404
Ashley Vu <ashleyvuhoo@gmail.com>
2018-07-30 15:58:39 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
 # dog.rb
+# class Dog
+#   def initialize(bark, sit, person, talk, walk)
+#     @bark = bark
+#     @sit = sit 
+#     @person = person 
+#     @talk = talk 
+#     @walk = walk 
+#   end 
+# end 
+#   dog_one = Dog.new("Woof!","The dog is sitting","Hello")
+# puts dog_one
+
+class Dog 
+  def 
